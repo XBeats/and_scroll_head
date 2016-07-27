43acbc39cae54c1cc05113c9342a2cfa0b1a4207
fenghaifeng <fhf11991@LY.com>
2016-07-27 11:15:23 +0800

    初始化提交

---
+++ app/src/main/java/com/aitangba/scrollhead/ScrollToTopBehavior.java
@@ -0,0 +1,56 @@
+package com.aitangba.scrollhead;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by fhf11991 on 2016/7/27.
+ */
+
+public class ScrollToTopBehavior extends CoordinatorLayout.Behavior<View>{
+    int offsetTotal = 0;
+    boolean scrolling = false;
+
+    public ScrollToTopBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, View child, View dependency) {
+        return true;
+    }
+
+    @Override
+    public boolean onDependentViewChanged(CoordinatorLayout parent, View child, View dependency) {
+        child.setY(dependency.getY()+dependency.getHeight());
+        return true;
+    }
+
+    @Override
+    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, View child, View directTargetChild, View target, int nestedScrollAxes) {
+        return false;
+    }
+
+    @Override
+    public void onNestedScroll(CoordinatorLayout coordinatorLayout, View child, View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
+        offset(child, dyConsumed);
+    }
+
+    public void offset(View child,int dy){
+        int old = offsetTotal;
+        int top = offsetTotal - dy;
+        top = Math.max(top, -child.getHeight());
+        top = Math.min(top, 0);
+        offsetTotal = top;
+        if (old == offsetTotal){
+            scrolling = false;
+            return;
+        }
+        int delta = offsetTotal-old;
+        child.offsetTopAndBottom(delta);
+        scrolling = true;
+    }
+
+}
